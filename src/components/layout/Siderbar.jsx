f5bd0362486059b5797cde9c98b56dc6ad2076e9
vojdel <vasquezpinedaj@gmail.com>
2021-07-02 15:51:39 -0400

    feat(menu): ✨ Update list of menu

--- src/components/layout/Siderbar.jsx
+++ src/components/layout/Siderbar.jsx
@@ -1,5 +1,5 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faHome, faKey, faTimes, faUserAlt, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {faAddressBook, faBlind, faChild, faHeadSideCough, faHome, faKey, faMale, faChalkboard, faChalkboardTeacher, faMapMarkedAlt, faSchool, faStethoscope, faTimes, faUserAlt, faUserCircle, faUserTie} from "@fortawesome/free-solid-svg-icons";
 
 const Siderbar = ({estilo}) => {
 
@@ -14,36 +14,175 @@ const Siderbar = ({estilo}) => {
           </a>
         </div>
         <hr className="horizontal dark mt-0" />
-        <div className="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+        <div className="collapse navbar-collapse w-auto h-auto" id="sidenav-collapse-main">
           <ul className="navbar-nav">
+            <li className="nav-item mt-3">
+              <h6 className="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6 text-white">Maestros</h6>
+            </li>
             <li className="nav-item">
-              <a href="#" className="nav-link active">
+              <a href="#" className="nav-link active" data-bs-toggle="collapse" href="#collapseDireccion" role="button" aria-expanded="false" aria-controls="collapseDireccion">
                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                  <FontAwesomeIcon icon={faHome} />
+                  <FontAwesomeIcon icon={faAddressBook} />
                 </div>
-                <span className="nav-link-text ms-1">Item 1</span>
+                <span className="nav-link-text ms-1">Dirección</span>
               </a>
             </li>
+            <ul className="navbar-nav collapse" id="collapseDireccion">
+              <li className="nav-item">
+                <a href="#" className="nav-link ps-4">
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3">
+                    <FontAwesomeIcon icon={faMapMarkedAlt} />
+                  </div>
+                  <span className="nav-link-text ms-1">Estado</span>
+                </a>
+              </li>
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faMapMarkedAlt} />
+                  </div >
+                  <span className="nav-link-text ms-1">Municipio</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faMapMarkedAlt} />
+                  </div >
+                  <span className="nav-link-text ms-1">Parroquia</span >
+                </a >
+              </li >
+            </ul >
             <li className="nav-item">
-              <a href="" className="nav-link">
+              <a href="#" className="nav-link" data-bs-toggle="collapse" href="#collapsePersona" role="button" aria-expanded="false" aria-controls="collapsePersona">
                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                  <FontAwesomeIcon icon={faHome} />
+                  <FontAwesomeIcon icon={faMale} />
                 </div>
-                <span className="nav-link-text ms-1">Item 1</span>
+                <span className="nav-link-text ms-1">Personas</span>
+              </a>
+            </li>
+            <ul className="navbar-nav collapse" id="collapsePersona" >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faChild} />
+                  </div >
+                  <span className="nav-link-text ms-1" > estudiante</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faUserTie} />
+                  </div >
+                  <span className="nav-link-text ms-1" > empleado</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faUserTie} />
+                  </div >
+                  <span className="nav-link-text ms-1" > representante</span >
+                </a >
+              </li >
+            </ul >
+            <li className="nav-item">
+              <a href="" className="nav-link" data-bs-toggle="collapse" href="#collapseEnfermedades" role="button" aria-expanded="false" aria-controls="collapseEnfermedades">
+                <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <FontAwesomeIcon icon={faStethoscope} /></div>
+                <span className="nav-link-text ms-1">Enfermedades</span>
+              </a>
+            </li>
+            <ul className="navbar-nav collapse" id="collapseEnfermedades" >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faHeadSideCough} />
+                  </div >
+                  <span className="nav-link-text ms-1">Tipo de Alergias</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faHeadSideCough} />
+                  </div >
+                  <span className="nav-link-text ms-1">Alergias</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4 w-100">
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faBlind} />
+                  </div>
+                  <span className="nav-link-text ms-1">Tipo de Discapacidad</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faBlind} />
+                  </div >
+                  <span className="nav-link-text ms-1">Discapacidad</span >
+                </a>
+              </li>
+            </ul>
+            <li className="nav-item">
+              <a href="" className="nav-link" data-bs-toggle="collapse" href="#collapseEscuela" role="button" aria-expanded="false" aria-controls="collapseEscuela">
+                <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <FontAwesomeIcon icon={faSchool} /></div>
+                <span className="nav-link-text ms-1">Escuela</span>
+              </a>
+            </li>
+            <ul className="navbar-nav collapse" id="collapseEscuela">
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faSchool} />
+                  </div >
+                  <span className="nav-link-text ms-1">Salon</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faSchool} />
+                  </div >
+                  <span className="nav-link-text ms-1">Grado</span >
+                </a >
+              </li >
+              <li className="nav-item" >
+                <a href="#" className="nav-link ps-4" >
+                  <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center ms-3" >
+                    <FontAwesomeIcon icon={faSchool} />
+                  </div >
+                  <span className="nav-link-text ms-1">Periodo Escolar</span >
+                </a>
+              </li>
+            </ul>
+            <li className="nav-item mt-3">
+              <h6 className="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6 text-white"> Procesos</h6>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <FontAwesomeIcon icon={faChalkboard} /></div>
+                <span className="nav-link-text ms-1">Horarios</span>
               </a>
             </li>
             <li className="nav-item">
               <a href="" className="nav-link">
                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                  <FontAwesomeIcon icon={faHome} /></div>
-                <span className="nav-link-text ms-1">Item 1</span>
+                  <FontAwesomeIcon icon={faChalkboardTeacher} /></div>
+                <span className="nav-link-text ms-1">Asistencias</span>
               </a>
             </li>
             <li className="nav-item">
               <a href="" className="nav-link">
                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                  <FontAwesomeIcon icon={faHome} /></div>
-                <span className="nav-link-text ms-1">Item 1</span>
+                  <FontAwesomeIcon icon={faChalkboardTeacher} /></div>
+                <span className="nav-link-text ms-1">Permiso</span>
               </a>
             </li>
             <li className="nav-item mt-3">
@@ -73,10 +212,10 @@ const Siderbar = ({estilo}) => {
                 <span className="nav-link-text ms-1">Sign Up</span>
               </a>
             </li>
-          </ul>
-        </div>
-      </div>
-    </aside>
+          </ul >
+        </div >
+      </div >
+    </aside >
   );
 }
 
